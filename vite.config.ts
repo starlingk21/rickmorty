dbdb6c894149b9ce12cc5f57f55779207c5230af
starlingk21 <starlingk21@gmail.com>
2025-01-28 15:10:13 +0200

    feature(Setup): Packages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      vue()
+    ],
+    
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      dedupe: ['vue', 'vue-router', 'pinia']
+    },
+  }
 })
