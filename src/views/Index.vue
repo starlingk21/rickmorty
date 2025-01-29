74cd38045984566a4c95a5b6f4c4bd2ef15f97e5
starlingk21 <starlingk21@gmail.com>
2025-01-29 13:33:11 +0200

    feature(charactersList): Implemented api request, styled component for the list for desktop

---
+++ src/views/Index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+	import CharactersList from '@/components/list/CharactersList.vue'
+</script>
+
+<template>
+	<div class="characters-container">
+		<CharactersList />
+	</div>
+</template>
+
+<style scoped lang="scss">
+	.characters-container {
+		max-width: 1440px;
+		margin: 64px auto;
+	}
+</style>
